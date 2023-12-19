eb083e6029d5ad3e81ad68c7e87f8b1366cc9f70
lukejianu <71729102+lukejianu@users.noreply.github.com>
2023-12-19 13:40:33 -0800

    feat: setup bootable disk image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let hello: &[u8] = b"Hello, world!";
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in hello.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop {}
 }
