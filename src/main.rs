51211574481d9b8d9648f91c273ff56b0f14f44d
lukejianu <71729102+lukejianu@users.noreply.github.com>
2023-12-19 13:44:34 -0800

    fix: reorder local definitions

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let hello: &[u8] = b"Hello, world!";
     let vga_buffer = 0xb8000 as *mut u8;
+    let hello: &[u8] = b"Hello, world!";
     for (i, &byte) in hello.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
